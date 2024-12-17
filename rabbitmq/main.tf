edbb9ac87c44b61abbddd40411507966517409b5
koyunco <info@koyunco.com>
2024-12-17 22:32:38 +0100

    asdasd

--- rabbitmq/main.tf
+++ rabbitmq/main.tf
@@ -1,4 +1,17 @@
-resource "docker_image" "rabbitmq" {
+terraform {
+  required_providers {
+    podman = {
+      source  = "hashicorp/podman"
+      version = "~> 1.0.0"
+    }
+  }
+}
+
+provider "podman" {
+  uri = "unix:///run/podman/podman.sock"
+}
+
+resource "podman_image" "rabbitmq" {
   name = "custom_rabbitmq:latest"
   build {
     context    = "${path.module}"
@@ -6,9 +19,9 @@ resource "docker_image" "rabbitmq" {
   }
 }
 
-resource "docker_container" "rabbitmq" {
+resource "podman_container" "rabbitmq" {
   name  = "rabbitmq_ssl"
-  image = docker_image.rabbitmq.latest
+  image = podman_image.rabbitmq.latest
   ports {
     internal = 5671
     external = 5671
